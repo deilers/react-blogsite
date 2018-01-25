16e733220bc9ecda8eb4d0601494f8bedb89a7cb
Devon Eilers <devon.d.eilers@gmail.com>
2018-01-24 19:28:14 -0800

    update new posts component

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -3,29 +3,71 @@ import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
 
-    renderTitleField(field) {
+    renderField(field) {
         return (
-            <div>
+            <div className="form-group"> 
+                <label>{field.label}</label>
                 <input
+                    className="form-control"
                     type="text"
                     {...field.input}
                 />
+                {field.meta.error}
             </div>
-        )
+        );
+    }
+
+
+    onSubmit(values) {
+        console.log(values);
     }
 
     render() {
+
+        const { handleSubmit} = this.props;
+
         return (
-            <form>
+            <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
                 <Field
+                    label="Title"
                     name="title" 
-                    component={this.renderTitleField}
+                    component={this.renderField}
                 />
+                <Field
+                    label="Categories"
+                    name="categories"
+                    component={this.renderField}
+                />
+                <Field
+                    label="Post Content"
+                    name="content"
+                    component={this.renderField}
+                />
+                <button type="submit" className="btn btn-primary">Submit</button>
             </form>
         );
     }
 }
 
+function validate(values) {
+    const errors = {};
+
+    if (!values.title || values.title.length < 3) {
+        errors.title = "Enter a title that is at least 3 characters";
+    }
+
+    if (!values.categories) {
+        errors.categories = 'Enter some categories';
+    }
+
+    if(!values.content) {
+        errors.content = 'Enter some content please';
+    }
+
+    return errors;
+}
+
 export default reduxForm({
+    validate,
     form: 'PostsNewForm'
 })(PostsNew)
